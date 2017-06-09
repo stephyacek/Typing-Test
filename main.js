b5bbeae9aec9e58a4e446c7dc0e87e3a168b42d3
Stephanie Yacek <stephyacek@gmail.com>
2017-06-09 13:38:22 -0700

    Added JS variables. #1

---
+++ main.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+var $enterPangram = document.querySelector('#enterPangram')
+var $pangram = document.querySelector('#pangram')
+
+$pangram.addEventListener('keydown', copyText)
+
+function copyText() {
+
+}
