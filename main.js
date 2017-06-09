0fc6a0396c8f4f0f7403eab3cfae6d5a6cb5baef
Stephanie Yacek <stephyacek@gmail.com>
2017-06-09 15:52:27 -0700

    Split string into array. #3

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 /* eslint-disable no-unused-vars */
-var $enterPangram = document.querySelector('#enterPangram')
+
 var $pangram = document.querySelector('#pangram')
 
-$pangram.addEventListener('keydown', copyText)
+$pangram.textContent = 'Jaded zombies acted quaintly but kept driving their oxen forward.'
 
-function copyText() {
-
-}
+var splitString = $pangram.textContent.split('')
