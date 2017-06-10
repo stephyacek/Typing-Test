0a70a0ad38d76fbc151b8acb79d988dd92ba19ea
Stephanie Yacek <stephyacek@gmail.com>
2017-06-09 17:48:57 -0700

    Add span containers. #3

--- main.js
+++ main.js
@@ -4,4 +4,11 @@ var $pangram = document.querySelector('#pangram')
 
 $pangram.textContent = 'Jaded zombies acted quaintly but kept driving their oxen forward.'
 
-var splitString = $pangram.textContent.split('')
+var letters = $pangram.textContent.split('')
+
+
+function createLetter(letter) {
+  var $letter = document.createElement('span')
+  $pangram.appendChild($letter)
+  $pangram.textContent = letter
+}
