9a9ef6445064632df5f1bab984e99e40b8db828c
Stephanie Yacek <stephyacek@gmail.com>
2017-06-11 16:18:25 -0700

    Added styling of first letter indicating that it's next. #4

--- main.js
+++ main.js
@@ -8,12 +8,19 @@ var letters = typeThis.split('')
 
 document.addEventListener('DOMContentLoaded', createPrompt(letters))
 
+var $firstChild = $pangram.children[0]
+
+$firstChild.setAttribute('class', 'active')
+
+//creating span with one character inside
 function createLetter(character) {
   var $pan = document.createElement('span')
   $pan.textContent = character
   return $pan
 }
-
+//Looping through the characters in array split which
+//calls the createLetter() and then finishes by
+//appending to the actual sentence
 function createPrompt(sentence) {
   for (var i = 0; i < sentence.length; i++) {
     var zombie = createLetter(sentence[i])
