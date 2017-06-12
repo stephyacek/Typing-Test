c6daa197e4a2ebd804b53e112147ecd971081834
Stephanie Yacek <stephyacek@gmail.com>
2017-06-12 09:28:57 -0700

    Added CSS class styling. #5

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 /* eslint-disable no-unused-vars */
-var $enterPangram = document.querySelector('#enterPangram')
+
 var $pangram = document.querySelector('#pangram')
 
-$pangram.addEventListener('keydown', copyText)
+var typeThis = 'Jaded zombies acted quaintly but kept driving their oxen forward.'
 
-function copyText() {
+var letters = typeThis.split('')
 
+document.addEventListener('DOMContentLoaded', createPrompt(letters))
+
+var $firstChild = $pangram.children[0]
+
+$firstChild.setAttribute('class', 'active')
+
+//creating span with one character inside
+function createLetter(character) {
+  var $pan = document.createElement('span')
+  $pan.textContent = character
+  return $pan
+}
+//Looping through the characters in array split which
+//calls the createLetter() and then finishes by
+//appending to the actual sentence
+function createPrompt(sentence) {
+  for (var i = 0; i < sentence.length; i++) {
+    var zombie = createLetter(sentence[i])
+    $pangram.appendChild(zombie)
+  }
 }
